b863e0de6c5ef1795ab1807fc75c5705dfe1740f
euaragon <eugenio.aragon@gmail.com>
2022-11-26 18:22:08 -0300

    formulario ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,76 @@
 import React from "react";
 import "./About.css";
-import { NavLink } from "react-router-dom";
-import imagen from "./img.png"
+
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+export const validate = (usuario) => {
+  let errors = {};
+  if (!regexEmail.test(usuario.username)) errors.username = "Debe ser un correo electrónico";
+  if (!usuario.password) errors.password = "Se requiere una constraseña";
+
+  return errors;
+};
 
 const Home = () => {
-    return(
-        <>
-         <div className="masabajo2">
-         <h1>THE RICK AND MORTY APP</h1>
-         <br/>
-        <NavLink to="/cards">CARDS</NavLink>
-        <NavLink to="/about">ABOUT</NavLink>
-        <img className="imagen1" src={imagen} alt="imagen"></img>
+  const [usuario, setUsuario] = React.useState({ username: "", password: "" });
 
-         </div>
-        
-        </>
-    )
-}
+  const [errors, setErrors] = React.useState({ username: "", password: "" });
+
+  const handleChange = (event) => {
+    setUsuario({ ...usuario, [event.target.name]: event.target.value });
+    setErrors(
+      validate({
+        ...usuario,
+        [event.target.name]: event.target.value,
+      })
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const arrErrors = Object.keys(errors);
+    if (arrErrors.length === 0) {
+      alert("Datos completos");
+      setUsuario({ username: "", password: "" });
+
+      setErrors({ username: "", password: "" });
+
+    } else { alert("Debes corregir los errores") }
+  };
+
+  return (
+    <>
+      <div className="masabajo2">
+        <h1>THE RICK AND MORTY APP</h1>
+
+        <br />
+        <h2>Logueate para entrar</h2>
+        {/* <NavLink to="/cards">CARDS</NavLink>
+        <NavLink to="/about">ABOUT</NavLink> */}
+        <form onSubmit={handleSubmit}>
+          <label>USERNAME: </label>
+          <input
+            onChange={handleChange}
+            name="username"
+            value={usuario.username}
+            placeholder="ingresá tu usuario..."
+            type="text"
+          ></input>
+          <p className="danger">{errors.username}</p>
+          <label>PASSWORD: </label>
+          <input
+            onChange={handleChange}
+            name="password"
+            value={usuario.password}
+            placeholder="ingresá tu contraseña..."
+            type="text"
+          ></input>
+          <p className="danger">{errors.password}</p>
+          <button>Entrar</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Home;
